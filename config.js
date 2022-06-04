f2430e408a0058924802f448cbed3659e8d369a9
Zhal122 <106859781+Zhal122@users.noreply.github.com>
2022-06-04 17:35:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['919971107409', 'Valor_Bot', true]
+  ['6283816271576', 'ZhaaLLBot', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = ['919971107409'] // Want some help?
-global.prems = ['919971107409'] // Premium user has unlimited limit
+global.mods = ['6283816271576'] // Want some help?
+global.prems = ['6283816271576'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -28,8 +28,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Valor Bot'
-global.author = 'Dinesh_Valor🇮🇳'
+global.packname = 'ZhaaLL Bott'
+global.author = 'XhaaLL_Valor🇮🇳'
 
 global.multiplier = 69 // The higher, The harder levelup
 
